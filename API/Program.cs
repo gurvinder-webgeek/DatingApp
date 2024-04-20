81f458df2a830dd85459951e2eb61275a01bcd8f
gurvinder-webgeek <gurvinder.webgeek@gmail.com>
2024-04-21 02:56:30 +0530

    second commit

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
 );
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 
 
